792e1a23dc04cef8b2ad7c4421bc667057e68e5b
mohammadrizki <mohammadrizki@10.20.230.234>
2019-07-05 20:34:29 +0800

    Typo fix Greyed out

--- src/simrskhanza/frmUtama.java
+++ src/simrskhanza/frmUtama.java
@@ -1,13 +1,13 @@
 /*
   Dilarang keras menggandakan/mengcopy/menyebarkan/membajak/mendecompile
   Software ini dalam bentuk apapun tanpa seijin pembuat software
-  (Khanza.Soft Media). Bagi yang sengaja membajak softaware ini ta
-  npa ijin, kami sumpahi sial 1000 turunan, miskin sampai 500 turu
-  nan. Selalu mendapat kecelakaan sampai 400 turunan. Anak pertama
+  (Khanza.Soft Media). Bagi yang sengaja membajak software ini tanpa
+  ijin, kami sumpahi sial 1000 turunan, miskin sampai 500 turunan.
+  Selalu mendapat kecelakaan sampai 400 turunan. Anak pertama
   nya cacat tidak punya kaki sampai 300 turunan. Susah cari jodoh
   sampai umur 50 tahun sampai 200 turunan. Ya Alloh maafkan kami
-  karena telah berdoa buruk, semua ini kami lakukan karena kami ti
-  dak pernah rela karya kami dibajak tanpa ijin.
+  karena telah berdoa buruk, semua ini kami lakukan karena kami tidak
+  pernah rela karya kami dibajak tanpa ijin.
  */
 
 package simrskhanza;
